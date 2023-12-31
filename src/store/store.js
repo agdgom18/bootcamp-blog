bdb31c2f8e7d02d8ccee416b08a782078a62775d
agdgom18 <agdgomelashvili18@gmail.com>
2023-12-31 17:50:29 +0400

    feat: add typescript

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,4 @@
 import { configureStore } from '@reduxjs/toolkit';
-// import subscribe from './subsSlice/subscribeSlice';
 import dataSlice from './dataSlice';
 import categoriesSlice from './categoriesSlice';
 import currentBlogSlice from './currentBlogSlice';
